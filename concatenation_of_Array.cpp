a00842481bbde8f5de6843da6d844444f2254303
Mahmud Hasan Shanto <130807335+CoderShanto@users.noreply.github.com>
2025-08-14 01:41:00 +0600

    Add files via upload

---
+++ concatenation_of_Array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans;
+        for(int i = 0; i<2; ++i){
+            for(int num:nums){
+                ans.push_back(num);
+            }
+        }
+        return ans;
+    }
+};
